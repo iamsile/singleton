051d2f304656cb934d07ba5db8aa5423e9af9d87
sile <iamsai007@sina.com>
2013-11-05 21:28:42 +0800

    singleton

--- singleton/main.cpp
+++ singleton/main.cpp
@@ -7,12 +7,54 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class singleton_t {
+public:
+	static singleton_t *instance() {
+		if (p_instance_ == 0) {
+			static singleton_t instance;
+			p_instance_ = &instance;
+		}
+		return p_instance_;
+	}
+	void count_increase() {
+		cnt++;
+	}
+	int count() const {
+		return cnt;
+	}
+private:
+	singleton_t():cnt(0) {}
+	~singleton_t() {}
+	static singleton_t *p_instance_;
+	int cnt;
+};
+
+class singleton {
+public:
+	static singleton *instance() {
+		if (p_instance_ == 0) {
+			static singleton instance;
+			p_instance_ = &instance;
+		}
+		return p_instance_;
+	}
+private:
+	singleton() {
+		singleton_t::instance()->count_increase();
+	}
+	~singleton() {}
+	static singleton *p_instance_;
+};
+
+singleton *singleton::p_instance_ = 0;
+singleton_t *singleton_t::p_instance_ = 0;
 
 int main(int argc, const char * argv[])
 {
-
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	singleton_t::instance();
+	cout << "---" << singleton_t::instance()->count()<< endl;
     return 0;
 }
 
