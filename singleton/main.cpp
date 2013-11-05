2d66089d35c0ff9a0b834331ac349b0711f42f2d
sile <iamsai007@sina.com>
2013-11-05 20:29:12 +0800

    Initial Commit

---
+++ singleton/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  singleton
+//
+//  Created by taowei on 13-11-5.
+//  Copyright (c) 2013年 taowei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
